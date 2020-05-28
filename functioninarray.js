ddf91a8b33a1062629cf0a8954e80c07143af5cf
Raman-3r <65964716+Raman-3r@users.noreply.github.com>
2020-05-28 11:03:36 +0530

    Add files via upload

---
+++ functioninarray.js
@@ -0,0 +1,12 @@
+var name= []
+function rmn(){
+    name.push("raman");
+    name.push("aman");
+    name.push("ritu");
+    name.push("shreya");
+    name.pop("ritu");
+    
+}
+rmn();
+rmn();
+console.log(name);
